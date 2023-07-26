fb7feefa19604f4bda9079e0fc802bb2bcfeaf2b
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2023-07-27 01:18:56 +0530

    product delete
    product controller work done,
    now will work on error handling

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -60,3 +60,21 @@ exports.updateProduct=async(req,res,next)=>{
     })
 
 }
+
+//delete product=>/api/v1/admin/product/:id
+exports.deleteProduct=async(req,res,next)=>{
+    const product=await Product.findById(req.params.id)
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message:'cant find the product'
+        })
+    } 
+
+    await product.deleteOne()
+
+    res.status(200).json({
+        success:true,
+        message:'Product is deleted'
+    })
+}
--- back-end/routes/product.js
+++ back-end/routes/product.js
@@ -1,12 +1,14 @@
-const express=require("express")
-const router=express.Router()
+const express = require("express")
+const router = express.Router()
 
-const{getProducts,newProduct, getsingleProduct,updateProduct}=require('../controllers/productController.js')
+const { getProducts, newProduct, getsingleProduct, updateProduct, deleteProduct } = require('../controllers/productController.js')
 
 router.route('/products').get(getProducts);
 router.route("/product/:id").get(getsingleProduct)
 
 router.route('/admin /product/new').post(newProduct)
-router.route("/admin/product/:id").put(updateProduct)
 
+router.route("/admin/product/:id")
+                .put(updateProduct)
+                .delete(deleteProduct)
 module.exports = router;
